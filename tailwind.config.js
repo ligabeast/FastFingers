ab72a91e430925ba66b904644fa32f7bfe38b039
Baran <Baran_ozbey02@outlook.de>
2023-05-21 18:43:51 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      cagliostro: ["Cagliostro", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
