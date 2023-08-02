9ab2f29423f6bf7b2537c33b809611467bd14af6
Baran <Baran_ozbey02@outlook.de>
2023-08-02 23:06:45 +0200

    added frontend for leaderboard

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="flex flex-col space-x-4 max-w-4xl w-full">
+  <div class="flex flex-col space-y-4 max-w-4xl w-full">
     <div class="flex flex-row justify-between font-semibold">
       <p>Daily Leaderboard</p>
       <p class="text-neutral-500">Resets in 2 hours</p>
     </div>
-    <table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>User</th>
-          <th>WPM</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Baran</td>
-          <td>139</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Armin</td>
-          <td>134</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="font-semibold">
+      <div class="flex flex-row px-4 py-2 rounded-md bg-neutral-900">
+        <p class="w-[20%]">Rank</p>
+        <p class="w-[70%]">User</p>
+        <p class="w-[10%]">WPM</p>
+      </div>
+      <div class="flex flex-row px-4 py-2 rounded-md">
+        <p class="w-[20%]">1</p>
+        <p class="w-[70%]">Baran</p>
+        <p class="w-[10%]">100</p>
+      </div>
+      <div class="flex flex-row px-4 py-2 rounded-md bg-neutral-900">
+        <p class="w-[20%]">2</p>
+        <p class="w-[70%]">Armin</p>
+        <p class="w-[10%]">31</p>
+      </div>
+    </div>
   </div>
 </template>
